dd3e7273965f3ec5b922870539562bd75fd8d4d3
Dana Luukko and Michelle Morin <michelle@>
2020-03-03 15:00:13 -0800

    finish testing using red/green/refactor process

--- Anagram.Tests/ModelTests/WordTests.cs
+++ Anagram.Tests/ModelTests/WordTests.cs
@@ -46,9 +46,9 @@ namespace Anagram.Tests
     {
       //Arrange
       List<string> words = new List<string> {};
-      words.Add("beard");
-      Word inputWord = new Word("bread", words);
-      string myWord = "bread";
+      words.Add("b");
+      Word inputWord = new Word("b", words);
+      string myWord = "b";
       
       //Act
       char[] charArray = myWord.ToCharArray();
@@ -63,9 +63,9 @@ namespace Anagram.Tests
     {
       //Arrange
       List<string> anagrams = new List<string> {};
-      anagrams.Add("beard");
+      anagrams.Add("b");
       Word inputWordObject = new Word("bread", anagrams);
-      string possibleAnagram = "beard";
+      string possibleAnagram = "b";
       List<char[]> listOfPossibleAnagrams = new List<char[]> {};
 
       //Act
@@ -76,6 +76,75 @@ namespace Anagram.Tests
       //Assert
       CollectionAssert.AreEqual(listOfSplitAnagrams[0], listOfPossibleAnagrams[0]);
     }
+    [TestMethod]
+    public void WordToCharArray_SortList_List()
+    {
+      //Arrang 
+      List<string> words = new List<string> {};
+      words.Add("bread");
+      Word inputWord = new Word("bread", words);
+      string myWord = "bread";
 
+      //Act
+      char[] charArray = myWord.ToCharArray();
+      Array.Sort(charArray);
+      char[] myCharArray = inputWord.WordToCharArray();
+      
+      //Assert
+      CollectionAssert.AreEqual(charArray, myCharArray);
+    }
+    [TestMethod]
+    public void WordListToCharArray_SortListOfCharArrays_List()
+    {
+      //Arrange
+      List<string> anagrams = new List<string> {};
+      anagrams.Add("beard");
+      Word inputWordObject = new Word("bread", anagrams);
+      string possibleAnagram = "beard";
+      List<char[]> listOfPossibleAnagrams = new List<char[]> {};
+
+      //Acct
+      List<char[]> listOfSplitAnagrams = inputWordObject.WordListToCharArrays();
+      char[] splitAnagram = possibleAnagram.ToCharArray();
+      Array.Sort(splitAnagram);
+      listOfPossibleAnagrams.Add(splitAnagram);
+
+      //Assert
+      CollectionAssert.AreEqual(listOfSplitAnagrams[0], listOfPossibleAnagrams[0]);
+    }
+
+    [TestMethod]
+    public void WordConstructor_MakesEmptyList_List()
+    {
+      //Arrange
+      List<string> anagrams = new List<string> {};
+      anagrams.Add("b");
+      Word inputWordObject = new Word("bread", anagrams);
+      List<string> comparisonList = new List<string> {};
+
+      //Act
+      List<string> listOfActualAnagrams = inputWordObject.ActualAnagrams;
+
+      //Assert
+      CollectionAssert.AreEqual(comparisonList, listOfActualAnagrams);
+    }
+
+    [TestMethod]
+    public void IsWordAnagram_ReturnsListOfWords_List()
+    {
+      //Arrange
+      List<string> words = new List<string> {};
+      words.Add("beard");
+      Word inputWord = new Word("bread", words);
+
+      //Act
+      char[] wordToArray = inputWord.WordToCharArray();
+      List<char[]> wordListToArray = inputWord.WordListToCharArrays();
+      inputWord.IsWordAnagram(wordToArray, wordListToArray);
+
+      List<string> actualAnagrams = inputWord.ActualAnagrams;
+      //Assert
+      CollectionAssert.AreEqual(words, actualAnagrams);
+    }
   }
 }
--- Anagram/Models/Word.cs
+++ Anagram/Models/Word.cs
@@ -6,17 +6,20 @@ namespace Anagram.Models
   public class Word
   {
     public string InputWord { get; set; }
+    public List<string> ActualAnagrams { get; set; } = new List<string> {};
     public List<string> PossibleAnagrams { get; } = new List<string> {};
 
     public Word(string inputWord, List<string> possibleAnagrams)
     {
       InputWord = inputWord;
       PossibleAnagrams = possibleAnagrams;
+      ActualAnagrams = new List<string> {};
     }
 
     public char[] WordToCharArray()
     {
       char[] inputWordArray = InputWord.ToCharArray();
+      Array.Sort(inputWordArray);
       return inputWordArray;
     }
 
@@ -26,9 +29,23 @@ namespace Anagram.Models
       foreach (string currentWord in PossibleAnagrams)
       {
         char[] charArray = currentWord.ToCharArray();
+        Array.Sort(charArray);
         result.Add(charArray);
       }
       return result;
+    }
+
+    public void IsWordAnagram(char[] wordArray, List<char[]> inputWordsArrays)
+    {
+      string sortedWord = new String(wordArray);
+      for(int i=0; i < inputWordsArrays.Count; i++)
+      {
+        string sortedListWord = new String(inputWordsArrays[i]);
+        if (sortedWord == sortedListWord)
+        {
+          ActualAnagrams.Add(PossibleAnagrams[i]);
+        }
+      }
     }
   }
 }
