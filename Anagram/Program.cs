858e61a713737254de642cbdc5d15cfa7ae0cb4a
Dana Luukko and Michelle Morin <michelle@>
2020-03-03 15:57:00 -0800

    build UI for checking whether words are anagrams

--- Anagram/Program.cs
+++ Anagram/Program.cs
@@ -1,5 +1,6 @@
 using Anagram.Models;
 using System;
+using System.Collections.Generic;
 
 namespace Anagram
 {
@@ -7,7 +8,47 @@ namespace Anagram
   {
     public static void Main()
     {
+      Console.WriteLine("Enter a Word :");
+      string userInputWord = Console.ReadLine();
+      List<string> possibleAnagrams = EnterPossibleAnagrams(userInputWord);
+      Word userWord = new Word(userInputWord, possibleAnagrams);
+      DisplayAnagrams(userWord);
+    }
 
+    public static List<string> EnterPossibleAnagrams(string userInputWord)
+    {
+      Console.WriteLine("Enter possible anagrams for" + userInputWord + " seperated by a space:");
+      string userInputPossibleAnagram = Console.ReadLine();
+      List<string> listOfAnagramOptions = new List<string> {};
+
+      string[] arrayOfOptions = userInputPossibleAnagram.Split(" ");
+
+      foreach (string item in arrayOfOptions)
+      {
+        listOfAnagramOptions.Add(item);
+      }
+      return listOfAnagramOptions;
+    }
+
+    public static void DisplayAnagrams(Word userWord)
+    {
+      char[] userWordArray = userWord.WordToCharArray();
+      List<char[]> possibleAnagramList = userWord.WordListToCharArrays();
+
+      userWord.IsWordAnagram(userWordArray, possibleAnagramList);
+
+      List<string> actualAnagrams = userWord.ActualAnagrams;if (actualAnagrams.Count == 0)
+      {
+        Console.WriteLine("No anagrams here!");
+      }
+      else
+      {
+        Console.WriteLine("The following are anagrams: ");
+        foreach (string anagram in actualAnagrams)
+        {
+          Console.WriteLine(anagram);
+        }
+      }
     }
   }
 }
